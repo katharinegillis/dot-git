a1c35495ef5e153e3a47a76afe49ca4e2377c100
Katharine Gillis <kgillis@fieldsafesolutions.com>
2021-01-10 17:02:52 -0700

    Change to input for git config

--- run.sh
+++ run.sh
@@ -4,23 +4,15 @@ ELLIPSIS_SRC=$1
 
 source $ELLIPSIS_SRC/init.bash
 
-load git
-
-git.configured() {
-    for key in user.name user.email github.user; do
-        if [ -z "$(git config --global $key | cat)"  ]; then
-            return 1
-        fi
-    done
-    return 0
-}
+#load git
 
 # Install git configuration
-git.add_include '~/.gitinclude'
+#git.add_include '~/.gitinclude'
 
-git.configured || cat <<\EOF
-You should set your email, name and github user for git with `git config`:
-git config --global user.name "John Smith"
-git config --global user.email "johnsmith@example.com"
-git config --global github.user "johnsmith"
-EOF
+for key in user.name user.email github.user; do
+    if [ -z "$(git config --global $key | cat)"  ]; then
+        echo "Please enter your git $key:"
+        read var
+        git config --global $key "$var"
+    fi
+done
